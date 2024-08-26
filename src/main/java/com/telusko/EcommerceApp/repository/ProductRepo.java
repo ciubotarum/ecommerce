dcd214073ccab85251d928713a3c180037c07665
ciubotarum <ciubotarum@outlook.com>
2024-08-26 19:46:13 +0300

    Create a Product repository

---
+++ src/main/java/com/telusko/EcommerceApp/repository/ProductRepo.java
@@ -0,0 +1,10 @@
+package com.telusko.EcommerceApp.repository;
+
+import com.telusko.EcommerceApp.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+
+}
