c5d7542292553fbee7fde8ed08c4036180f3cb05
ciubotarum <ciubotarum@outlook.com>
2024-08-27 12:39:52 +0300

    Add a new columns to the table and a no args constructor

--- src/main/java/com/telusko/EcommerceApp/model/Product.java
+++ src/main/java/com/telusko/EcommerceApp/model/Product.java
@@ -1,18 +1,37 @@
 package com.telusko.EcommerceApp.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Data    // gives getters and setters
 @AllArgsConstructor   // creates a constructor from all variables
+@NoArgsConstructor
 @Component
+@Entity
 public class Product {
-    private int prodId;
-    private String ProdName;
-    private int price;
 
-    public Product() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
 
-    }
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+
+    @Lob   // for large data
+    private byte[] imageDate;
 }
